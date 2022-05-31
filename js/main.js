1cc33b77eaa32553d7dd84a504501cd7d47af6b6
Fl4v10-F3L1X <flaviofrancisco1802@hotmail.com>
2022-05-31 16:32:07 +0100

    adicionando animacoes a tag nav(menu de navegacao)

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@
 const navMenu = document.getElementById('nav-menu'),
         navToggle = document.getElementById('nav-toggle'),
         navClose = document.getElementById('nav-close')
+
+/*======== MENU SHOW ========*/
+/* validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*======== MENU HIDDEN ========*/
+/* validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
