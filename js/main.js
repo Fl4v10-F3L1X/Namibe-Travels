15301d635d119feb8ca6bb2b5520d24168f3be0f
Fl4v10-F3L1X <flaviofrancisco1802@hotmail.com>
2022-05-31 16:41:20 +0100

    animacao para mostrar o butão subir

--- js/main.js
+++ js/main.js
@@ -86,3 +86,12 @@ function finalVideo() {
 
 //ended, whe the video ends
 videoFile.addEventListener('ended', finalVideo)
+
+
+/*=========== SHOW SCROLL UP===========*/
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    //When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with
+    if (this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');    
+}
+window.addEventListener('scroll', scrollUp)
