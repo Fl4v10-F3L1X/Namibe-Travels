24657af0bb92a4db000eece96aac3a7c1516aa08
Fl4v10-F3L1X <flaviofrancisco1802@hotmail.com>
2022-06-07 11:16:36 +0100

    Add comentários para melhor percepcão do código

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
-/*=========== SHOW MENU ==========*/
+/*=========== MOSTRAR MENU ==========*/
 const navMenu = document.getElementById('nav-menu'),
         navToggle = document.getElementById('nav-toggle'),
         navClose = document.getElementById('nav-close')
 
-/*======== MENU SHOW ========*/
-/* validate if constant exists */
+/*========  MOSTRAR MENU ========*/
+/* validar se a constante existe */
 if(navToggle){
     navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
 }
 
-/*======== MENU HIDDEN ========*/
-/* validate if constant exists */
+/*======== ESCONDER MENU ========*/
+/* validar se a constante existe */
 if (navClose) {
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
 
-/*====== REMOVE MENU MOBILE ======*/
+/*====== REMOVER MENU MÓVEL ======*/
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    //When we click on each nav__link, we remove the show-menu class
+    //Quando clicamos em cada link de navegação, removemos a classe show-menu
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*====== CHANGE BACKGROUND HEADER ======*/
+/*====== ALTERAR CABEÇALHO DE FUNDO ======*/
 function scrollHeader(){
     const header = document.getElementById('header')
-    //when the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    //quando a rolagem for maior que 100 de altura da janela de visualização, adicione a classe scroll-header à tag de cabeçalho
     if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*====== SWIPER DISCOVER ======*/
+/*====== SWIPER DESCOBERTA ======*/
 var swiper = new Swiper(".discover__container", {
     effect: "coverflow",
     grabCursor: true,
@@ -60,17 +60,17 @@ const videoFile = document.getElementById('video-file'),
 
 function playPause(){
     if (videoFile.paused) {
-        //Play video
+        //Reproduzir vídeo
         videoFile.play()
 
-        // We change the icon
+        // Mudamos o ícone
         videoIcon.classList.add('ri-pause-line')
         videoIcon.classList.remove('ri-play-line')
     }else{
-        //Pause video
+        //Pausar vídeo
         videoFile.pause()
 
-        //We change the icon
+        //Mudamos o ícone
         videoIcon.classList.remove('ri-pause-line')
         videoIcon.classList.add('ri-play-line')
     }
@@ -79,24 +79,24 @@ function playPause(){
 videoButton.addEventListener('click', playPause)
 
 function finalVideo() {
-    //video ends, icon change
+    //vídeo termina, mudança de ícone
     videoIcon.classList.remove('ri-pause-line')
     videoIcon.classList.add('ri-play-line')
 }
 
-//ended, whe the video ends
+//terminou, quando o vídeo termina
 videoFile.addEventListener('ended', finalVideo)
 
 
-/*=========== SHOW SCROLL UP===========*/
+/*=========== MOSTRAR ROLAGEM PARA CIMA ===========*/
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
-    //When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with
+    //Quando a rolagem for maior que 200 de altura da janela de visualização, adicione a classe show-scroll à tag
     if (this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');    
 }
 window.addEventListener('scroll', scrollUp)
 
-/*============ SCROLL SECTION ACTIVE LINK ============*/
+/*============ LINK ATIVO DA SEÇÃO DE ROLAGEM ============*/
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
@@ -116,37 +116,37 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
-/*================ DARK LIGHT THEME ==============*/
+/*================ TEMA LUZ ESCURO ==============*/
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'ri-sun-line'
 
-//Previously selected topic (if user selected)
+//Tópico selecionado anteriormente (se o usuário for selecionado)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-//We obtain the current theme that the interface has by validating the dark-theme class
+//Obtemos o tema atual que a interface possui validando a classe dark-theme
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
 
-//We validate if the user previously chose a topic
+//Validamos se o usuário já escolheu um tópico
 if(selectedTheme){
-    //If the validation is fulfilled, we ask what the issue was to know if we activated or desactived the dark
+    //Se a validação for cumprida, perguntamos qual foi o problema para saber se ativamos ou desativamos o escuro
     document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
     themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
 }
 
-// Active / desactive the theme manually with the button
+// Ative/desative o tema manualmente com o botão
 themeButton.addEventListener('click', () => {
-    //Add or remove the dark / icon theme
+    //Adicione ou remova o tema escuro / ícone
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // we save the theme and the current icon that the user choose
+    // salvamos o tema e o ícone atual que o usuário escolher
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-theme', getCurrentIcon())
 })
 
-/*============ SCROLL REVEAL ANIMATION ===========*/
+/*============ ROLAGEM DE ANIMAÇÃO DE REVELAÇÃO ===========*/
 const sr = ScrollReveal({
     distance: '60px',
     duration: 2800,
